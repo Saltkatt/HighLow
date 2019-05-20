1845bb5d3e7f49fe6ad602d46043ad7bf6b7af21
Elin Sexton <elin.sexton@gmail.com>
2019-05-20 14:33:49 +0200

    minor adjustment

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -120,29 +120,29 @@ div {
   grid-row: 5;
   box-sizing: border-box;
 }
-        #numberField {
-            margin: auto;
-            padding: 16px 32px;
-            width: 60%;
-            border-radius: 8px;
-            box-sizing: border-box;
-            font-size: 16px;
-            cursor: pointer;
-        }
+#numberField {
+  margin: auto;
+  padding: 16px 32px;
+  width: 60%;
+  border-radius: 8px;
+  box-sizing: border-box;
+  font-size: 16px;
+  cursor: pointer;
+}
         
-        #okButton {
-            background-color: gold;
-            border: 1px solid black;
-            border-radius: 8px;
-            padding: 16px 32px;
-            width:25%;
-            display: inline-block;
-            font-size: 16px;
-            margin: auto;
-            cursor: pointer;
-        }
-        #okButton:hover {
-            /* brightness(0.4);  /* 40% brightness */
-            filter: brightness(120%);
-        }
+#okButton {
+  background-color: gold;
+  border: 1px solid black;
+  border-radius: 8px;
+  padding: 16px 32px;
+  width:25%;
+  display: inline-block;
+  font-size: 16px;
+  margin: auto;
+  cursor: pointer;
+  }
+#okButton:hover {
+  /* brightness(0.4);  /* 40% brightness */
+  filter: brightness(120%);
+}
 </style>
