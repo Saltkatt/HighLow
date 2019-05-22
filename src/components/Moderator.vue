a368e0bdac2c23e214d3268581e87a483040c8ed
joeliths <your>
2019-05-22 14:09:17 +0200

    solved conflict

--- src/components/Moderator.vue
+++ src/components/Moderator.vue
@@ -37,17 +37,6 @@ export default {
         {
           rule3: "The game will continue until someone has answered correctly."
         }
-<<<<<<< HEAD
-    },
-    methods:{
-        
-        //talk() conveys information such hiLow() respons, rules, and phrases 
-        //which allows moderator to interact with player.
-         talk: function(msg){
-
-            return msg
-        },
-=======
       ],
       phrases: [
         {
@@ -72,7 +61,6 @@ export default {
     talk: function(msg) {
       return msg;
     },
->>>>>>> e113d1b69f0365bad480c7a6286c6d7e61023244
 
     onBtnClick() {
       this.count++;
@@ -128,47 +116,19 @@ export default {
       return this.$store.state.activePlayers;
     },
 
-<<<<<<< HEAD
-        //Retrieves answer from answer()
-        //Receives guess from XXX and checks correct, too high or too low. Sends response.
-        hiLow: function(guess){
-            var respons = null;
-            var answer = this.answers();
-            if(guess == answer){
-                respons = "Correct!";
-            }
-            else if(guess < answer) {
-                respons = "Too low!";
-                this.$store.commit('setLowestNumber', guess)
-            }
-            else if (guess > answer) {
-                respons = "Too high";
-                this.$store.commit('setHighestNumber', guess)
-            } 
-        
-            return this.talk(respons)
-        },
-=======
     getHighestGuess() {
       return this.$store.state.highestNumber;
     },
->>>>>>> e113d1b69f0365bad480c7a6286c6d7e61023244
 
     getLowestGuess() {
       return this.$store.state.lowestNumber;
     },
-<<<<<<< HEAD
-    computed: mapGetters(['getQuestionBank', 'getGuess'])
-    
-}
-=======
 
     getCorrectAnswer() {
       return this.$store.state.questionBank[0].answer;
     }
   }
 };
->>>>>>> e113d1b69f0365bad480c7a6286c6d7e61023244
 </script>
 
 <style>
@@ -203,21 +163,12 @@ export default {
   grid-row: 1;
   text-align: left;
 }
-<<<<<<< HEAD
-.modContainer{
-    background-color: aquamarine;
-=======
 .modContainer {
   background-color: aquamarine;
->>>>>>> e113d1b69f0365bad480c7a6286c6d7e61023244
 }
 img {
   float: center;
   width: 40%;
   height: 40% ;
 }
-<<<<<<< HEAD
 </style>
-=======
-</style>
->>>>>>> e113d1b69f0365bad480c7a6286c6d7e61023244
