e31b2932e8f54d1c9d889a47e6fb463ca2942064
martinwesterlund <44970859+martinwesterlund@users.noreply.github.com>
2019-06-03 13:33:41 +0200

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -154,6 +154,14 @@ export const store = new Vuex.Store({
   },
 
   mutations: {
+      
+      //toggle avatar selection
+        toggleAvatar: function (state, avatarId) {
+        for(let i= 0; i < state.avatars.length; i++){
+        state.avatars[i].selected = false;
+      }
+      state.avatars[avatarId-1].selected = !state.avatars[avatarId-1].selected;
+    },
 
     //reset active players list
     resetActivePlayers: function (state) {
